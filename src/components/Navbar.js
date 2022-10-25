1a8cafdf3bc7ae78d9640ec4c9e3906fd978046e
Josephine <josephine.closan@yahoo.com>
2022-10-25 23:28:35 +0300

    style button in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import '../css/Navbar.css'
 import React from 'react';
 import MenuList from './MenuList'
 import {useState} from 'react'
+import { Button} from "react-bootstrap";
 
 
 const Navbar = (props) => {
@@ -10,15 +11,17 @@ const Navbar = (props) => {
     setIsOpen(!isOpen);
   }
   
-  // const size = {
-  //   fontSize: "20rem"
-  // }
+  const style = {
+    marginLeft: "2rem",
+    marginBottom: "1rem"
+  }
   
-  // const title = "Amazing scientists"
+  
   return (
     <nav>
       <h2 className='title'>Amazing scientists</h2>
-      <button onClick={handleClick} className="navbar-button">See more</button>
+      
+      <Button variant="dark" onClick={handleClick} style={style}>See more</Button>
        {isOpen && <MenuList/>}
     </nav>
   );
