0275c0d34831462c5cb2bf4a6853facc3bee1319
Josephine <josephine.closan@yahoo.com>
2022-10-25 11:15:48 +0300

    add description to the scientists' object

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -19,7 +19,7 @@ const Profile = () => {
                     <div>
                         <img src={profile.src} width="200"/>
                         <h3>{profile.name}</h3>
-                        <h4>Ea elit aliqua ut id commodo anim adipisicing sunt quis. Deserunt sint ad consectetur esse tempor voluptate. Cupidatat eu ea amet amet ipsum mollit pariatur officia nulla est adipisicing. Pariatur consequat dolor esse culpa et nulla cillum sunt. Duis nulla mollit incididunt duis laboris adipisicing velit esse. Reprehenderit aute labore pariatur tempor deserunt velit sit duis voluptate dolor ex. Occaecat est cillum tempor anim aliqua aliquip do cupidatat.</h4>
+                        <h4>{profile.description}</h4>
                         <p>Fields: {profile.subject}</p>
                         <p>Source: <a href={profile.link} target="_blank"
                         rel="noreferrer noopener">Wikipedia</a></p>
--- src/components/scientists.js
+++ src/components/scientists.js
@@ -5,6 +5,7 @@ export const scientistsData = [
     src: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Einstein_1921_by_F_Schmutzer_-_restoration.jpg  ",
     link: "https://en.wikipedia.org/wiki/Albert_Einstein",
     subject: "Physics",
+    description: "Ea elit aliqua ut id commodo anim adipisicing sunt quis. Deserunt sint ad consectetur esse tempor voluptate. Cupidatat eu ea amet amet ipsum mollit pariatur officia nulla est adipisicing. Pariatur consequat dolor esse culpa et nulla cillum sunt. Duis nulla mollit incididunt duis laboris adipisicing velit esse. Reprehenderit aute labore pariatur tempor deserunt velit sit duis voluptate dolor ex. Occaecat est cillum tempor anim aliqua aliquip do cupidatat."
   },
   {
     id: 2,
@@ -12,6 +13,8 @@ export const scientistsData = [
     src: "https://upload.wikimedia.org/wikipedia/commons/c/c8/Marie_Curie_c._1920s.jpg",
     link: "https://en.wikipedia.org/wiki/Marie_Curie",
     subject: "Radioactivity",
+    description: "Ea elit aliqua ut id commodo anim adipisicing sunt quis. Deserunt sint ad consectetur esse tempor voluptate. Cupidatat eu ea amet amet ipsum mollit pariatur officia nulla est adipisicing. Pariatur consequat dolor esse culpa et nulla cillum sunt. Duis nulla mollit incididunt duis laboris adipisicing velit esse. Reprehenderit aute labore pariatur tempor deserunt velit sit duis voluptate dolor ex. Occaecat est cillum tempor anim aliqua aliquip do cupidatat."
+
   },
   {
     id: 3,
@@ -19,6 +22,8 @@ export const scientistsData = [
     src: "https://upload.wikimedia.org/wikipedia/commons/7/79/Tesla_circa_1890.jpeg",
     link: "https://en.wikipedia.org/wiki/Nikola_Tesla",
     subject: "Radioactivity",
+    description: "Ea elit aliqua ut id commodo anim adipisicing sunt quis. Deserunt sint ad consectetur esse tempor voluptate. Cupidatat eu ea amet amet ipsum mollit pariatur officia nulla est adipisicing. Pariatur consequat dolor esse culpa et nulla cillum sunt. Duis nulla mollit incididunt duis laboris adipisicing velit esse. Reprehenderit aute labore pariatur tempor deserunt velit sit duis voluptate dolor ex. Occaecat est cillum tempor anim aliqua aliquip do cupidatat."
+
   },
   {
     id: 4,
@@ -26,6 +31,8 @@ export const scientistsData = [
     src: "https://upload.wikimedia.org/wikipedia/commons/7/71/Roentgen2.jpg",
     link: "https://en.wikipedia.org/wiki/Wilhelm_R%C3%B6ntgen",
     subject: "Physics",
+    description: "Ea elit aliqua ut id commodo anim adipisicing sunt quis. Deserunt sint ad consectetur esse tempor voluptate. Cupidatat eu ea amet amet ipsum mollit pariatur officia nulla est adipisicing. Pariatur consequat dolor esse culpa et nulla cillum sunt. Duis nulla mollit incididunt duis laboris adipisicing velit esse. Reprehenderit aute labore pariatur tempor deserunt velit sit duis voluptate dolor ex. Occaecat est cillum tempor anim aliqua aliquip do cupidatat."
+
   },
   {
     id: 5,
@@ -33,6 +40,8 @@ export const scientistsData = [
     src: "https://upload.wikimedia.org/wikipedia/commons/2/2e/Charles_Darwin_seated_crop.jpg",
     link: "https://en.wikipedia.org/wiki/Charles_Darwin",
     subject: "Natural history, geology",
+    description: "Ea elit aliqua ut id commodo anim adipisicing sunt quis. Deserunt sint ad consectetur esse tempor voluptate. Cupidatat eu ea amet amet ipsum mollit pariatur officia nulla est adipisicing. Pariatur consequat dolor esse culpa et nulla cillum sunt. Duis nulla mollit incididunt duis laboris adipisicing velit esse. Reprehenderit aute labore pariatur tempor deserunt velit sit duis voluptate dolor ex. Occaecat est cillum tempor anim aliqua aliquip do cupidatat."
+
   },
   {
     id: 6,
@@ -41,6 +50,8 @@ export const scientistsData = [
     link: "https://en.wikipedia.org/wiki/Isaac_Newton",
     subject:
       "Physics, natural philosophy, alchemy, theology, mathematics, astronomy, economics",
+      description: "Ea elit aliqua ut id commodo anim adipisicing sunt quis. Deserunt sint ad consectetur esse tempor voluptate. Cupidatat eu ea amet amet ipsum mollit pariatur officia nulla est adipisicing. Pariatur consequat dolor esse culpa et nulla cillum sunt. Duis nulla mollit incididunt duis laboris adipisicing velit esse. Reprehenderit aute labore pariatur tempor deserunt velit sit duis voluptate dolor ex. Occaecat est cillum tempor anim aliqua aliquip do cupidatat."
+
   },
   {
     id: 7,
@@ -48,6 +59,8 @@ export const scientistsData = [
     src: "https://upload.wikimedia.org/wikipedia/commons/2/27/Fran%C3%A7oise_Foliot_-_Mus%C3%A9e_Pasteur_-_Portrait_de_Jean-Joseph_Pasteur.jpg",
     link: "https://en.wikipedia.org/wiki/Louis_Pasteur",
     subject: "Biology, Microbiology, Chemistry, Mathematics, Physics",
+    description: "Ea elit aliqua ut id commodo anim adipisicing sunt quis. Deserunt sint ad consectetur esse tempor voluptate. Cupidatat eu ea amet amet ipsum mollit pariatur officia nulla est adipisicing. Pariatur consequat dolor esse culpa et nulla cillum sunt. Duis nulla mollit incididunt duis laboris adipisicing velit esse. Reprehenderit aute labore pariatur tempor deserunt velit sit duis voluptate dolor ex. Occaecat est cillum tempor anim aliqua aliquip do cupidatat."
+
   },
   {
     id: 8,
@@ -55,5 +68,7 @@ export const scientistsData = [
     src: "https://upload.wikimedia.org/wikipedia/commons/d/d4/Justus_Sustermans_-_Portrait_of_Galileo_Galilei%2C_1636.jpg",
     link: "https://en.wikipedia.org/wiki/Galileo_Galilei",
     subject: "Astronomy, physics, engineering, natural philosophy, mathematics",
+    description: "Ea elit aliqua ut id commodo anim adipisicing sunt quis. Deserunt sint ad consectetur esse tempor voluptate. Cupidatat eu ea amet amet ipsum mollit pariatur officia nulla est adipisicing. Pariatur consequat dolor esse culpa et nulla cillum sunt. Duis nulla mollit incididunt duis laboris adipisicing velit esse. Reprehenderit aute labore pariatur tempor deserunt velit sit duis voluptate dolor ex. Occaecat est cillum tempor anim aliqua aliquip do cupidatat."
+
   },
 ];
